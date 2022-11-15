b0e815f73acde2d625dbd66d63adcb69c8bc15b1
Fitz <clement@lahoche.info>
2022-11-15 09:27:17 +0100

    add openMP v2

--- src/experimentsDef.py
+++ src/experimentsDef.py
@@ -1,4 +1,4 @@
-def getExpDetails():
+def getImplementationDetails():
     return {
         "baseline" : {
             "folder" : "micro-aevol2",
@@ -10,10 +10,15 @@ def getExpDetails():
             "nameCuda" : "micro_aevol_gpu",
             "nameCpu" : "micro_aevol_cpu"
         },
+        "openMPMutationV2" : {
+            "folder" : "openMPMutationV2",
+            "nameCuda" : "micro_aevol_gpu",
+            "nameCpu" : "micro_aevol_cpu"
+        },
     }
 
 
-def getImplementationDetails():
+def getExpDetails():
     return {
             # base project
         "baseline" : {
@@ -51,6 +56,25 @@ def getImplementationDetails():
 
         # openMPMutationV1
         "openMPMutationV1" : {
+            "perDefaultCPU" : {
+                "executable" : "nameCpu",
+                "arg" : [
+
+                ]
+            },
+
+            "chargeCPU" : {
+                "executable" : "nameCpu",
+                "arg" : [
+                    "-w", "128",
+                    "-h", "128",
+                    "-g", "5000",
+                ]
+            }
+        },
+
+        # openMPMutationV2
+        "openMPMutationV2" : {
             "perDefaultCPU" : {
                 "executable" : "nameCpu",
                 "arg" : [
